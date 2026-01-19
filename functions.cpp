6489be7f79c85e03617732ab1b24d84affb51ce9
Ajay Gottipati <2100030182@kluniversity.in>
2026-01-19 11:55:43 -0600

    Add mutex to synchronize washing process

--- functions.cpp
+++ functions.cpp
@@ -1,7 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 #include "student.h"
+
+mutex washMutex;
+
 void washStudent(Student s) {
-    cout << "Student " << s.id << " is washing" << endl;
+    washMutex.lock();
+    cout << "Student " << s.id << " started washing" << endl;
+    washMutex.unlock();
+
     this_thread::sleep_for(chrono::seconds(s.washingTime));
+
+    washMutex.lock();
+    cout << "Student " << s.id << " finished washing" << endl;
+    washMutex.unlock();
 }
